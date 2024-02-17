67149e27bf347d7e1fe2dc2d232f2d0ff8dfd1c2
Nikita Tiurikov <tiurikovnikita@gmail.com>
2024-02-17 16:25:04 +0200

    Second commit(exchange USD)

--- convert.py
+++ convert.py
@@ -2,6 +2,9 @@
 def money_converter(summa: float, convert_from: str, convert_to: str):
     exchange = {'UAH': {'USD': 38.2,
                         'EUR': 41.4},
+                'USD': {'UAH': 37.6,
+                        'EUR': 1.08},
+
                 }
 
     return f'{summa * exchange[convert_from][convert_to]:.2f}'
